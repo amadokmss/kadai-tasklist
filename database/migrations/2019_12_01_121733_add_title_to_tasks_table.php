299ed76204846ea210ae0b19c06d1f7c38e4428f
EC2 Default User <ec2-user@ip-172-31-80-105.ec2.internal>
2019-12-09 07:33:30 +0000

    m1

--- database/migrations/2019_12_01_121733_add_title_to_tasks_table.php
+++ database/migrations/2019_12_01_121733_add_title_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToTasksTable extends Migration
+class AddTitleToTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,7 +16,6 @@ class AddStatusToTasksTable extends Migration
         Schema::table('tasks', function (Blueprint $table) {
             $table -> string("status",10);
             $table->integer('user_id')->unsigned()->index();
-            $table->string('jjj',10);
             
             $table->foreign('user_id')->references('id')->on('users');
         });
